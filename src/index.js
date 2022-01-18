0ba297cf845007744db9a8e509e18deae9a7b4f5
Aung Myo Khant <aungmyokhant2465@gmail.com>
2022-01-18 15:17:48 +0630

    create and get QR code and get transcation history

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const authLink = setContext((_, { headers }) => {
+  const loggedAuthorJSON = window.localStorage.getItem('loggedMerchant')
+  const result = JSON.parse(loggedAuthorJSON)
+  return {
+    headers: {      
+      ...headers,      
+      Authorization: result ? `Bearer ${result.token}` : null,
+    }
+  }
+})
+
+const httpLink = new HttpLink({ uri: 'https://api.aicpass.com/v1/graphql' })
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: authLink.concat(httpLink)
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client} >
+    <Router>
+      <App />
+    </Router>
+  </ApolloProvider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
